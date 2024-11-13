dceadab6db6fc816a5e2774c0d16e5cfb0f15d7f
Florian Burger <florianburger03@gmail.com>
2024-11-13 13:04:02 +0100

    removed old / created new test logic and moved compilation logic from main.cpp

---
+++ src/compile.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include "handler/handler.hpp"
+#include "file/file.hpp"
+#include "lexing/abc.hpp"
+#include "parsing/abc.hpp"
+#include "semantics/source.hpp"
+#include "generate/generator_c.hpp"
+
+namespace Compiler {
+    inline bool compile(const Filepath& input) {
+        // handler
+        Handler handler;
+        // // :: test
+        // handler.push(testError());
+        // handler.push(testWarning());
+        // handler.flush([]([[maybe_unused]] strview code) {
+        //     return CodeSpan{ Location{ 0, 0 }, Location{ 0, 0 } };
+        // });
+
+        // file
+        File file(input);
+        // // :: test
+        // strview code(file.view().data() + 40, file.view().data() + 60);
+        // cout << code << endl;
+        // cout << file.searchSpan(code).value().begin.ln << endl;
+        // cout << file.searchSpan(code).value().begin.col << endl;
+        // cout << file.searchSpan(code).value().end.ln << endl;
+        // cout << file.searchSpan(code).value().end.col << endl;
+
+        // lexing
+        Lexer lexer = abcLexer();
+        List<Token> tokens = lexer.lex(file.view(), handler);
+        if (handler.shouldCancel()) {
+            handler.flush([&](strview code) {
+                return file.searchSpan(code).value();
+            });
+            return false;
+        }
+        // // :: test
+        // for (const Token& token : tokens) {
+        //     cout << token.name << " : " << token.view << endl;
+        // }
+
+        // parsing
+        Parser parser = abcParser();
+        UniquePtr<Layer> layer = parser.parse(move(tokens), handler);
+        if (handler.shouldCancel()) {
+            handler.flush([&](strview code) {
+                return file.searchSpan(code).value();
+            });
+            return EXIT_FAILURE;
+        }
+        // // :: test
+        // for (const Node& node : *layer) {
+        //     node.dump();
+        // }
+
+        // semantics
+        ABC::Source sourceABC(layer->front(), handler);
+        sourceABC.bind(handler);
+        if (handler.shouldCancel()) {
+            handler.flush([&](strview code) {
+                return file.searchSpan(code).value();
+            });
+            return false;
+        }
+        
+        // generate
+        IR::Source sourceIR;
+        GeneratorC generator(cout);
+        generator.generate(sourceIR);
+        
+        return true;
+    }
+}
---
+++ src/test.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include "compile.hpp"
+
+namespace Compiler {
+    static inline string check_mark = "\033[32m\u2714\033[0m";
+    static inline string cross_mark = "\033[31m\u2718\033[0m";
+    bool test_file(const Filepath& path) {
+        if (compile(path)) {
+            cout << check_mark << " " << path.string() << endl;
+            return true;
+        }
+        else {
+            cout << cross_mark << " " << path.string() << endl;
+            return false;
+        }
+    }
+
+    bool test(const Filepath& path) {
+        uint passed = 0;
+        uint failed = 0;
+        if (stdf::is_regular_file(path)) {
+            if (test_file(path)) {
+                passed += 1;
+            }
+            else {
+                failed += 1;
+            }
+        }
+        else if (stdf::is_directory(path)) {
+            for (const auto& entry : stdf::recursive_directory_iterator(path)) {
+                if (entry.is_regular_file()) {
+                    if (test_file(path)) {
+                        passed += 1;
+                    }
+                    else {
+                        failed += 1;
+                    }
+                }
+            }
+        }
+        if (failed == 0) {
+            cout << "** all " << passed << " tests passed successfully! **" << endl;
+        }
+        else {
+            cout << "** " << failed << " of " << passed + failed << " tests failed! **" << endl;
+        }
+        return failed == 0;
+    }
+}
